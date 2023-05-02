765e45a56320c59f87b7a287cbdaed21e3441c06
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-02 14:27:29 +0200

    add autotype function

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,63 @@
 <script>
 export default {
   name: "HomePage",
+
+  data() {
+    return {
+      textToType: "",
+      typeStatus: false,
+      typeArray: ["I'm Web Developer.", "I'm Full Stack Web Developer."],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+
+  methods: {
+    typeText() {
+      if (this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+
+        this.textToType += this.typeArray[this.typeArrayIndex].charAt(
+          this.charIndex
+        );
+        this.charIndex += 1;
+
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+
+        this.textToType = this.typeArray[this.typeArrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+
+        if (this.typeArrayIndex >= this.typeArray.length)
+          this.typeArrayIndex = 0;
+
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
+  },
+
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
 };
 </script>
 
@@ -21,8 +78,20 @@ export default {
       </div>
 
       <div class="row">
-        <div class="col-12 d-flex justify-content-center align-items-center">
-          <h1>Il mio Porfolio</h1>
+        <div
+          class="col-12 d-flex flex-column justify-content-center align-items-center"
+        >
+          <span class="d-block">Hi, my name is Gianmarco. </span>
+          <div>
+            <span class="typed-text">
+              {{ textToType }}
+            </span>
+            <span
+              class="my-cursor my-cursor-blink"
+              :class="{ typing: typeStatus }"
+              >&nbsp;</span
+            >
+          </div>
         </div>
       </div>
 
@@ -51,6 +120,22 @@ section {
 
   .col-12 {
     min-height: 400px;
+
+    span {
+      font-size: 2.5rem;
+      font-weight: 500;
+    }
+
+    span.my-cursor {
+      display: inline-block;
+      margin-left: 6px;
+      width: 2px;
+      background-color: #fff;
+    }
+
+    span.my-cursor.typing {
+      animation: none;
+    }
   }
 }
 </style>
